f10b8b674f89331da9b858abbc6bf6d6b2fbc791
Madis <madis1995@gmail.com>
2015-04-24 18:20:09 +0300

    esimene liikuv julla

---
+++ src/KeyboardControls.java
@@ -0,0 +1,8 @@
+/*Siia võiks teha klassi, mis loeb tekstifailist nuppude konfiguratsiooni
+ja vastavalt muudab siis nupud ära. Kui on error lugemisel, siis võiks
+kasutada default konfiguratsiooni(WASD ja nooled ehk?)
+
+enum kasutus?
+*/
+public class KeyboardControls {
+}
--- src/MainGame.java
+++ src/MainGame.java
@@ -2,12 +2,18 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.newdawn.slick.*;
+import org.newdawn.slick.geom.Circle;
+import org.newdawn.slick.geom.Shape;
 
 public class MainGame extends BasicGame
 {
     //isendiväljad
     private Image field;
     //private Music music;
+    private Shape player1;
+    float speed = 0.5f;
+    float x = 250;
+    float y = 250;
 
 
     public MainGame(String gamename)
@@ -21,15 +27,43 @@ public class MainGame extends BasicGame
         //MUUSIKA
         /*music = new Music("data/gamemusic.ogg"); //Frostbite(Original mix) by WarHector
         music.loop();*/
+        player1 = new Circle(x,y,25);//x,y,raadius
     }
 
     @Override
-    public void update(GameContainer gc, int i) throws SlickException {}
+    public void update(GameContainer gc, int delta) throws SlickException {
+        Input input = gc.getInput();
+        //control player1
+        //up
+        if(input.isKeyDown(Input.KEY_UP)){
+            player1.setY(y - speed * delta);
+            y = y - speed * delta;//delta makes the speed run the same on ANY computer
+        }
+        //down
+        if(input.isKeyDown(Input.KEY_DOWN)){
+            player1.setY(y + speed * delta);
+            y = y + speed * delta;
+        }
+        //left
+        if(input.isKeyDown(Input.KEY_LEFT)){
+            player1.setX(x - speed * delta);
+            x = x - speed * delta;
+        }
+        //right
+        if(input.isKeyDown(Input.KEY_RIGHT)){
+            player1.setX(x + speed * delta);
+            x = x + speed * delta;
+        }
+
+    }
 
     @Override
     public void render(GameContainer gc, Graphics g) throws SlickException
     {
+        //gc.setShowFPS(false);
         field.draw(0,0); //siin hoitakse mänguväli ees
+        g.setColor(Color.red);
+        g.fill(player1);
     }
 
     public static void main(String[] args)
@@ -38,6 +72,8 @@ public class MainGame extends BasicGame
         {
             AppGameContainer appgc;
             appgc = new AppGameContainer(new MainGame("Jalka"));
+            appgc.setTargetFrameRate(120);
+            //appgc.setVSync(true);
             appgc.setDisplayMode(1000, 500, false);
             appgc.start();
         }
