7d9e725f45e6208798128c425e3422765a3b3a6d
Madis <madis1995@gmail.com>
2015-04-24 21:18:08 +0300

    gamephysics class v1, dno kas töötab

--- src/GamePhysics.java
+++ src/GamePhysics.java
@@ -2,16 +2,22 @@ import org.newdawn.slick.geom.Vector2f;
 
 public class GamePhysics {
 
-    public Vector2f elasticCollisionDirection(Vector2f first, Vector2f second){
-        float resultX=0;
-        float resultY= 0;
+    public void elasticCollision(Player player, Player ball){
+        Vector2f relativeVelocity = new Vector2f(ball.getDx()-player.getDx(), ball.getDy()-player.getDy());
+        Vector2f normal = relativeVelocity.getNormal();
+        float velAlongNormal = relativeVelocity.dot(normal);
+        if (velAlongNormal>0){
+            return;
+        }
+        //impulse scalar
+        float j = -1*velAlongNormal;
+        j /= player.getInverseMass() + ball.getInverseMass();
+        Vector2f impulse = normal.scale(j);
+        Vector2f playerVelocity = impulse.scale(player.getInverseMass());
+        Vector2f ballVelocity = impulse.scale(ball.getInverseMass());
 
-        //CODE
-
-
-
-        Vector2f result = new Vector2f(resultX, resultY);
-        return result;
+        player.translateDxDy(-playerVelocity.getX(), -playerVelocity.getY());
+        ball.translateDxDy(ballVelocity.getX(), ballVelocity.getY());
     }
 
 }
--- src/Player.java
+++ src/Player.java
@@ -7,12 +7,37 @@ public class Player {
     Circle circle;
     private float dx; //kiirus
     private float dy;
+    private float inverseMass;
 
     Player(float x, float y, float r){
         this.circle = new Circle(x,y,r);
     }
+    Player(float x, float y, float r, float inverseMass){
+        this.circle = new Circle(x,y,r);
+        this.inverseMass = inverseMass;
+    }
 
+    public void setDx(float dx) {
+        this.dx = dx;
+    }
 
+    public void setDy(float dy) {
+        this.dy = dy;
+    }
+    public void translateDxDy(float dx,float dy){
+        this.dx += dx;
+        this.dy += dy;
+    }
 
+    public float getDx() {
+        return dx;
+    }
 
+    public float getDy() {
+        return dy;
+    }
+
+    public float getInverseMass() {
+        return inverseMass;
+    }
 }
